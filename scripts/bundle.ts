0f6eca0ae74963c872a141e2c919433f66f0d79c
Óscar Carrasco <oxcabe@gmail.com>
2024-02-06 14:29:37 +0000

    chore: set up package for npmjs publishing

--- scripts/bundle.ts
+++ scripts/bundle.ts
@@ -1,34 +1,33 @@
 import path from "path";
 import type { BuildConfig } from "bun";
+import dts from "bun-plugin-dts";
+
+const cwd = process.cwd();
 
 const baseBundleConfig: BuildConfig = {
   // TODO: get entryPoint from package.json "module" field
-  entrypoints: [path.resolve(process.cwd(), "./index.ts")],
+  entrypoints: [path.resolve(cwd, "./index.ts")],
   minify: true,
   splitting: true,
-  outdir: path.resolve(process.cwd(), "./build/dist/"),
+  outdir: path.resolve(cwd, "./dist/"),
+  plugins: [dts()],
 };
 
 const browserBundleConfig: BuildConfig = {
   ...baseBundleConfig,
   target: "browser",
-  naming: "[dir]/[name]-browser.[ext]",
-};
-
-const bunBundleConfig: BuildConfig = {
-  ...baseBundleConfig,
-  target: "bun",
-  naming: "[dir]/[name]-bun.[ext]",
+  naming: "[dir]/bombastic.umd.cjs",
 };
 
 const nodeBundleConfig: BuildConfig = {
   ...baseBundleConfig,
   target: "node",
-  naming: "[dir]/[name]-node.[ext]",
+  naming: "[dir]/bombastic-node.mjs",
 };
 
-const bundleConfigs = [browserBundleConfig, bunBundleConfig, nodeBundleConfig];
+const bundleConfigs = [browserBundleConfig, nodeBundleConfig];
 
+// Build all configurations
 for (const bundleConfig of bundleConfigs) {
   await Bun.build(bundleConfig);
 }
