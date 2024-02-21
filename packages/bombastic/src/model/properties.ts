a88b5fac77547f000cdbcff3efde1fc925bdfcc9
Óscar Carrasco <oxcabe@gmail.com>
2024-02-21 18:12:23 +0000

    chore: document code in model/ directory

--- packages/bombastic/src/model/properties.ts
+++ packages/bombastic/src/model/properties.ts
@@ -1,11 +1,35 @@
+/**
+ * Represents materials by their material name.
+ */
 export interface Material {
+  /**
+   * Name of the material.
+   */
   name: string;
 }
 
+/**
+ * Represents a designed object, its metadata and associated materials.
+ */
 export interface Object {
+  /**
+   * Name of the object.
+   */
   name: string;
+  /**
+   * Type of the object.
+   */
   type: string;
+  /**
+   * IFC type of the object.
+   */
   ifcType: string;
+  /**
+   * Global ID of the object.
+   */
   globalId: string;
+  /**
+   * List of materials associated to the object.
+   */
   materials: Material[];
 }
