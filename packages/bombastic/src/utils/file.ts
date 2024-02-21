5f0267011104372c97a5fb42acf4a68a4f803c5a
Óscar Carrasco <oxcabe@gmail.com>
2024-02-21 18:15:37 +0000

    chore: document code in utils/ directory

--- packages/bombastic/src/utils/file.ts
+++ packages/bombastic/src/utils/file.ts
@@ -1,5 +1,11 @@
 import { IfcAPI } from "web-ifc";
 
+/**
+ * Loads an IFC file given its path.
+ * @param ifcFilePath Path of the IFC file.
+ * @param ifcApi web-ifc IfcAPI object.
+ * @returns A promise with the model ID of the file.
+ */
 export const loadIfcFromFile = async (
   ifcFilePath: string | URL,
   ifcApi: IfcAPI,
